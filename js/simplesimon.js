7057932a912ae851d7443efc1aeb5a4ed4cecb10
Oscar Soto <oscarasotoa@gmail.com>
2016-10-31 12:16:58 -0500

    Add sounds for the sequence and the click selection. Fix bug - Game wasn't working the second time. Issue was that event listeners for the color boxes were created more than once.

--- js/simplesimon.js
+++ js/simplesimon.js
@@ -1,73 +1,90 @@
 (function () {
     'use strict';
 
-    var soundObject = document.createElement("audio");
-    soundObject.src= "http://www.pacdv.com/sounds/interface_sound_effects/sound93.wav";
-    soundObject.volume=0.9;
-    soundObject.autoPlay=false;
-    soundObject.preLoad=true;
-
+    // Variables
+    var boxes = ["#box1", "#box2", "#box3", "#box4"];
+    var sequenceSoundObject = document.createElement("audio");
+    var selectionSoundObject = document.createElement("audio");
+    var random = Math.floor(Math.random() * 4); // Generates fist random number
     var counter = 0;
     var sequence = [];
 
+    sequenceSoundObject.src= "http://www.pacdv.com/sounds/interface_sound_effects/beep-3.wav";
+    sequenceSoundObject.volume=0.9;
+    sequenceSoundObject.autoPlay=false;
+    sequenceSoundObject.preLoad=true;
+
+    selectionSoundObject.src= "http://www.pacdv.com/sounds/interface_sound_effects/sound93.wav";
+    selectionSoundObject.volume=0.9;
+    selectionSoundObject.autoPlay=false;
+    selectionSoundObject.preLoad=true;
+
+
+
+    // Functions
+
+    // setupGame() Removes start button and change the opacity of the colors, every time the game is played.
     function setupGame() {
-        var boxes = ["#box1", "#box2", "#box3", "#box4"];
+
         $("#startGameButton").fadeOut();
-        $("#message").remove();
+        $("#gameOverMessage").remove();
 
         boxes.forEach(function (box) {
             $(box).fadeTo("fast", 0.3);
         });
-        console.log(sequence);
+
+        // Reset variables in case user play more than once.
+        sequence = [];
+        counter = 0;
     }
 
+    // showSequence shows the sequence to the user for every round.
     function showSequence(sequence) {
-        var boxes = ["#box1", "#box2", "#box3", "#box4"];
-        $("#rounds").html("Round: <br><strong>" + sequence.length + "</strong>"); // # of rounds
+
+        $("#rounds").html("Round: <br><strong>" + sequence.length + "</strong>"); // Show the # of rounds
+
         // Show the Sequence
         var i = 0;
         var id = setInterval(function () {
+            // Boxes fade In and Out and play a sound according with the sequence.
             $(boxes[sequence[i]]).fadeTo("slow", 1).fadeTo("slow", 0.3);
-            if (i == sequence.length) {
+            if (i == sequence.length - 1) {
                 clearInterval(id);
             }
             i ++;
-            soundObject.play();
+            sequenceSoundObject.play();
         }, 1000);
     }
 
+    // gameOver() Shows message "Game Over", show the start button for the user to play again.
     function gameOver() {
-        var boxes = ["#box1", "#box2", "#box3", "#box4"];
-        var gameOverHtml = "<div class='alert alert-warning center-block' id='message' role='alert'><h3 class='text-center'><strong>Game Over !</strong>Please click Start Game again</h3></div>";
+        var gameOverHtml = "<div class='alert alert-warning center-block' id='gameOverMessage' role='alert'>" +
+                "<h3 class='text-center'><strong>Game Over !</strong> Please click Start Game again</h3></div>";
         $("#startGameButton").fadeIn();
         $("#gameOver").html(gameOverHtml);
         boxes.forEach(function (box) {
             $(box).fadeTo("fast", 1);
         });
+
     }
 
     function simpleSimonGame() {
-        var random = Math.floor(Math.random() * 4);
-
-        setupGame();
-        sequence.push(random);
-        showSequence(sequence);
-
+        // Event Listeners for the color boxes
         $(".box").each(function (indexBox, box) {
             box.addEventListener("click", function () {
                 // === Checks Sequence ===
                 if(indexBox == sequence[counter]){
+                // If user click is correct it will restore opacity momentarily to show the user that his selection was correct.
+                    selectionSoundObject.play();
                     $(box).fadeTo("fast", 1).animate({
                         opacity: 0.3
                     }, 500);
                     counter ++
                 } else {
-                    counter = 0;
-                    sequence = [];
                     gameOver();
-
                 }
                 if (sequence.length == counter) {
+                // If full sequence is correct it will generate a new random number and push it to the sequence.
                     random = Math.floor(Math.random() * 4);
                     sequence.push(random);
                     showSequence(sequence);
@@ -77,10 +94,15 @@
         });
     }
 
+    // Initialize event listeners
+    simpleSimonGame();
 
+    // Start Game Button
     $("#startGameButton").on("click", function () {
         event.preventDefault();
-        simpleSimonGame();
+        setupGame();
+        sequence.push(random);
+        showSequence(sequence);
     });
 
 })();
