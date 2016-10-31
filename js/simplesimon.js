3775747e9471e125902a3c64f14ce450cb44e277
Oscar Soto <oscarasotoa@gmail.com>
2016-10-31 10:44:09 -0500

    add CSS to create simple simon game shape

--- js/simplesimon.js
+++ js/simplesimon.js
@@ -1,21 +1,29 @@
 (function () {
     'use strict';
+
+    var soundObject = document.createElement("audio");
+    soundObject.src= "http://www.pacdv.com/sounds/interface_sound_effects/sound93.wav";
+    soundObject.volume=0.9;
+    soundObject.autoPlay=false;
+    soundObject.preLoad=true;
+
     var counter = 0;
     var sequence = [];
 
     function setupGame() {
         var boxes = ["#box1", "#box2", "#box3", "#box4"];
-        $("#startGame").fadeOut();
+        $("#startGameButton").fadeOut();
         $("#message").remove();
 
         boxes.forEach(function (box) {
             $(box).fadeTo("fast", 0.3);
         });
+        console.log(sequence);
     }
 
     function showSequence(sequence) {
         var boxes = ["#box1", "#box2", "#box3", "#box4"];
-        $("#rounds").html(sequence.length); // # of rounds
+        $("#rounds").html("Round: <br><strong>" + sequence.length + "</strong>"); // # of rounds
         // Show the Sequence
         var i = 0;
         var id = setInterval(function () {
@@ -24,14 +32,14 @@
                 clearInterval(id);
             }
             i ++;
+            soundObject.play();
         }, 1000);
-
     }
 
     function gameOver() {
         var boxes = ["#box1", "#box2", "#box3", "#box4"];
         var gameOverHtml = "<div class='alert alert-warning center-block' id='message' role='alert'><h3 class='text-center'><strong>Game Over !</strong>Please click Start Game again</h3></div>";
-        $("#startGame").fadeIn();
+        $("#startGameButton").fadeIn();
         $("#gameOver").html(gameOverHtml);
         boxes.forEach(function (box) {
             $(box).fadeTo("fast", 1);
@@ -44,8 +52,6 @@
         setupGame();
         sequence.push(random);
         showSequence(sequence);
-
-
 
         $(".box").each(function (indexBox, box) {
             box.addEventListener("click", function () {
@@ -72,7 +78,7 @@
     }
 
 
-    $("#startGame").on("click", function () {
+    $("#startGameButton").on("click", function () {
         event.preventDefault();
         simpleSimonGame();
     });
